474671f35e1efafc237d54baa3dc2bfa822914ae
reynaldo57 <reynaldo57@hotmail.com>
2022-10-07 23:40:38 -0500

    AnimatedCrossFade

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -7,74 +7,98 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   bool flagAnimAlign = true;
   bool flagAnimContainer = true;
-
-
+  bool flagAnimCross = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("example"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40
-            ),
-            // Hero(
-            //   tag: 'goku',
-            //   child: Image.asset('assets/images/goku.jpg'),
-            // ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //         context, MaterialPageRoute(builder: (context) => HeroPage()));
-            //   },
-            //   child: Text("HereWeGo"),
-            // )
-            // GestureDetector(
-            //   onTap: (){
-            //     flagAnimAlign = !flagAnimAlign;
-            //     setState(() {
-            //
-            //     });
-            //   },
-            //   child: Container(
-            //     width: 250,
-            //     height: 250,
-            //     color: Colors.greenAccent,
-            //     child: AnimatedAlign(
-            //       duration: Duration(milliseconds: 1500),
-            //       alignment: flagAnimAlign ? Alignment.topRight : Alignment.bottomLeft,
-            //       curve: Curves.bounceOut,
-            //       child: Image.asset("assets/images/goku.jpg", height: 100,),
-            //     ),
-            //   ),
-            // ),
-
-            GestureDetector(
-              onTap: (){
-                setState(() {
-                  flagAnimContainer = !flagAnimContainer;
-                });
-              },
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 1400),
-                height: flagAnimContainer ? 200 : 100,
-                width: flagAnimContainer ? 200 : 100,
-                color: Colors.greenAccent,
-                child: FlutterLogo(size: 60,),
-                curve: Curves.bounceOut,
-                alignment: flagAnimContainer ? Alignment.bottomLeft : Alignment.topRight,
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text("example"),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              // Hero(
+              //   tag: 'goku',
+              //   child: Image.asset('assets/images/goku.jpg'),
+              // ),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.push(
+              //         context, MaterialPageRoute(builder: (context) => HeroPage()));
+              //   },
+              //   child: Text("HereWeGo"),
+              // )
+              // GestureDetector(
+              //   onTap: (){
+              //     flagAnimAlign = !flagAnimAlign;
+              //     setState(() {
+              //
+              //     });
+              //   },
+              //   child: Container(
+              //     width: 250,
+              //     height: 250,
+              //     color: Colors.greenAccent,
+              //     child: AnimatedAlign(
+              //       duration: Duration(milliseconds: 1500),
+              //       alignment: flagAnimAlign ? Alignment.topRight : Alignment.bottomLeft,
+              //       curve: Curves.bounceOut,
+              //       child: Image.asset("assets/images/goku.jpg", height: 100,),
+              //     ),
+              //   ),
+              // ),
+
+              // GestureDetector(
+              //   onTap: (){
+              //     setState(() {
+              //       flagAnimContainer = !flagAnimContainer;
+              //     });
+              //   },
+              //   child: AnimatedContainer(
+              //     duration: Duration(milliseconds: 1400),
+              //     height: flagAnimContainer ? 200 : 100,
+              //     width: flagAnimContainer ? 200 : 100,
+              //     color: Colors.greenAccent,
+              //     child: FlutterLogo(size: 60,),
+              //     curve: Curves.bounceOut,
+              //     alignment: flagAnimContainer ? Alignment.bottomLeft : Alignment.topRight,
+              //   ),
+              // )
+
+              AnimatedCrossFade(
+                duration: Duration(seconds: 2),
+                firstChild: GestureDetector(
+                  onTap: (){
+                    flagAnimCross = !flagAnimCross;
+                    setState(() {
+
+                    });
+                  },
+                  child: FlutterLogo(
+                    size: 100,
+                    style: FlutterLogoStyle.horizontal,
+                  ),
+                ),
+                secondChild: GestureDetector(
+                  onTap: (){
+                    flagAnimCross = !flagAnimCross;
+                    setState(() {
+
+                    });
+                  },
+                  child: FlutterLogo(
+                    size: 100,
+                    style: FlutterLogoStyle.stacked,
+                  ),
+                ),
+                crossFadeState: flagAnimCross ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+              ),
+            ],
+          ),
+        ));
   }
 }
