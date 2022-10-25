17cf9b287f062dbc4d873370f95d841b395f4c17
reynaldo57 <reynaldo57@hotmail.com>
2022-10-24 23:14:59 -0500

    Tween 3

--- lib/page/tween_page.dart
+++ lib/page/tween_page.dart
@@ -36,7 +36,7 @@ class TweenPage extends StatelessWidget {
                   duration: Duration(milliseconds: 1000),
                   builder: (BuildContext context, value, _){
                     return Transform.translate(
-                      offset: Offset(0, -100 * value!),
+                      offset: Offset(0, -110 * value!),
                       child: Text(
                       "Batman",
                       style: TextStyle(
@@ -45,7 +45,29 @@ class TweenPage extends StatelessWidget {
                       ),
                       ),
                     );
-                  })
+                  }),
+              TweenAnimationBuilder<double?>(
+                  tween: Tween(begin: 1, end: 0),
+                  duration: Duration(milliseconds: 1500),
+                  builder: (BuildContext context, value, _){
+                    return Transform.translate(
+                      offset: Offset(-200 * value!, 0),
+                      child: Image.asset('assets/images/goku.jpg'),
+
+                    );
+                  }),
+
+              TweenAnimationBuilder<double?>(
+                  tween: Tween(begin: 1, end: 0),
+                  duration: Duration(milliseconds: 2000),
+                  builder: (BuildContext context, value, _){
+                    return Transform.translate(
+                      offset: Offset(0, 100 * value!),
+                      child: Text("En Viajes Falabella tienes todo lo que necesitas para tus vacaciones en un solo lugar. Descubre las mejores ofertas de PAQUETES para vivir experiencias inolvidables junto a Viajes Falabella. También te ofrecemos la atención personalizada de profesionales expertos en viajes y turismo que te pueden ayudar a encontrar las mejores ofertas para vivir experiencias inolvidables junto a Viajes Falabella. Si quieres hablar con un asesor, puedes visitarnos presencialmente en las sucursales de Viajes Falabella o llamar y comprar por teléfono. Otra forma en que Falabella Viajes Perú es perfecta para ti son nuestras formas de financiación. Disfruta tus vacaciones pagando en cuotas sin interés y aprovecha las mejores promociones con distintas tarjetas y bancos. ¡Con tantas opciones seguro encuentras la oferta que estabas buscando! Comprando un Paquete de Falabella Viajes obtienes en una sola compra, el vuelo + el hotel a un precio más económico que si lo comparas por separado. No importa cuál sea tu destino preferido, si buscas Paquetes a Brasil, a Cuba, Cancún o Punta Cana. Viajes Falabella Turismo tiene todo lo que buscas para hacer realidad el viaje de tus sueños. ¿Qué esperas? ¡Compra ya mismo un Paquete Turístico para empezar a vivir tus vacaciones junto con Viajes Falabella!"),
+
+
+                    );
+                  }),
             ],
           ),
         ),
