4ebe5f5d20ec9c6e5a860ea5f09a7aca5f73af25
reynaldo57 <reynaldo57@hotmail.com>
2022-10-24 22:52:53 -0500

    Tween 2

--- lib/page/tween_page.dart
+++ lib/page/tween_page.dart
@@ -11,18 +11,43 @@ class TweenPage extends StatelessWidget {
           "Example"
         ),
       ),
+      // body: Center(
+      //   child: TweenAnimationBuilder<Color?>(
+      //     duration: Duration(milliseconds: 1200),
+      //     tween: ColorTween(begin: Colors.redAccent, end: Colors.greenAccent),
+      //     builder: (BuildContext context, value, _){
+      //       print(value);
+      //       return Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: value,
+      //       );
+      //     },
+      //   ),
+      // ),
+
       body: Center(
-        child: TweenAnimationBuilder<Color?>(
-          duration: Duration(milliseconds: 1200),
-          tween: ColorTween(begin: Colors.redAccent, end: Colors.greenAccent),
-          builder: (BuildContext context, value, _){
-            print(value);
-            return Container(
-              width: 100,
-              height: 100,
-              color: value,
-            );
-          },
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            children: [
+              TweenAnimationBuilder<double?>(
+                  tween: Tween(begin: 1, end: 0),
+                  duration: Duration(milliseconds: 1000),
+                  builder: (BuildContext context, value, _){
+                    return Transform.translate(
+                      offset: Offset(0, -100 * value!),
+                      child: Text(
+                      "Batman",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    );
+                  })
+            ],
+          ),
         ),
       ),
     );
